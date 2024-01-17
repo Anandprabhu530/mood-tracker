2ea68b7e7085078414a20d6de9afc333d73b01b2
Anandprabhu530 <anandprabhu530@gmail.com>
2024-01-17 16:23:47 +0530

    Template for mood analysis

--- app/components/EntryEditor.tsx
+++ app/components/EntryEditor.tsx
@@ -17,13 +17,22 @@ const EntryEditor = ({ entry }) => {
 
   return (
     <div className="w-full h-full flex flex-col">
-      <div className="text-4xl pb-10 pl-8 border-b-[1px]">Your Content</div>
+      <div className="text-4xl pb-10 pl-8 border-b-[1px] ">Your Content</div>
       {isLoading && <div className="pl-8 text-lg pt-8">...Loading</div>}
-      <textarea
-        className="bg-[#2e2e2e] text-white outline-none resize-none text-xl w-[96%] h-[90%]  font-poppins m-8 p-8 rounded-xl shadow-xl"
-        value={data}
-        onChange={(e) => setData(e.target.value)}
-      />
+      <div className="w-full h-full grid grid-cols-3">
+        <textarea
+          className="bg-[#2e2e2e] text-white outline-none resize-none text-xl col-span-2 w-full h-full font-poppins m-8 p-8 rounded-xl shadow-xl"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+        />
+        <div className="cols-span-1 border-l-[1px] flex flex-col">
+          <div className="p-8 text-2xl font-semibold">Analysis</div>
+          <div>Summary</div>
+          <div>Sentiment</div>
+          <div>Mood</div>
+          <div>Postive or Negative</div>
+        </div>
+      </div>
     </div>
   );
 };
