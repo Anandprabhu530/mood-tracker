8efcd995776d2b5b7f7c9deb0c9e189f6086b0d1
Anandprabhu530 <anandprabhu530@gmail.com>
2024-01-20 21:34:49 +0530

    Added Chain to Gemini

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,17 +1,25 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
 import { StructuredOutputParser } from "langchain/output_parsers";
+import { ChatPromptTemplate } from "langchain/prompts";
 import { z } from "zod"
 
 const parser = StructuredOutputParser.fromZodSchema(
     z.object({
-        answer: z.string().describe("answer to the user's question"),
-        sources: z
-            .array(z.string())
-            .describe("sources used to answer the question, should be websites."),
+        mood: z.string().describe("what is the mood of the data"),
+        summary: z.string().describe("What is the summary of the input"),
+        Sentiment: z.string().describe("What is the sentiment of the input"),
+        Negativity: z.boolean().describe("Negative or not"),
     })
 );
 
 export const analyze_data = async (input) => {
     const model = new ChatGoogleGenerativeAI({ modelName: "gemini-pro", maxOutputTokens: 2048 })
-    const answer = await model.invoke(input);
+    const prompt = ChatPromptTemplate.fromMessages([["system", "You are a mood analyzer. Analyze and say the output."],
+    ["user", "{input}"],])
+    const chain = prompt.pipe(model)
+    const answer = await chain.invoke({
+        question: "What is the capital of France?",
+        format_instructions: parser.getFormatInstructions(),
+    });
+    console.log(answer);
 } 
